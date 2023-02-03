7a656289dc35e036b8aef1d846a2254e18d71409
subhash kumar yadav <110653851+Subhashkunaryadav@users.noreply.github.com>
2023-02-03 12:50:52 +0530

    Add files via upload

---
+++ Variables and data types.py
@@ -0,0 +1,81 @@
+message = 'Hello World'
+print(message)
+#or
+print('Hello world')
+print('Hello python crash course!!')
+
+#String
+name='python langauage'
+print(name.title())
+
+message = 'i told my friend, "python is my favorite language"'
+"one of python's strengths is its diverse and supportive community."
+print(message.title())
+
+#Change string to upper or lower case
+name = 'python'
+print(name.upper())
+print(name.lower())
+
+first_name = ' subhash '
+second_name = ' yadav '
+full_name = (first_name) + (second_name)
+print(full_name.title())
+
+languages = ('\njava \npython \nhtml \nc language')
+print(languages)
+
+print (('words:\n\tbelive \n\tyourself \n\tasus \n\tlenovo').title())
+
+
+#avoid syntax error apostrophe(')
+message = "This can't be the best"
+print(message)
+
+#TIY
+message = 'Hello Eric, would you like to learn python'
+print(message)
+
+name = 'eric'
+print(name.upper())
+print(name.lower())
+print(name.title())
+
+quote = 'Albert Einstein said,"a person who never made a mistake \nnever tried anything new"'
+print(quote)
+
+names = '\nasus \nlenovo \nhp \nbelive'
+print(names.title())
+
+#Numbers
+add = 1 + 2
+print(add)
+
+subtraction = 1 - 2
+print(subtraction)
+
+multiplication = 1 * 2
+print(multiplication)
+
+division = 1 / 2
+print(division)
+
+add = 1 + 2.0
+print(add)
+
+x = float(input("Enter the first number:"))
+y = float(input("Enter the second number:"))
+addition = x+y
+subtraction = x-y
+multiplication = x*y
+division = x/y
+print('addition:', addition)
+print('subtraction:', subtraction)
+print('multiplication:', multiplication)
+print('division:', division)
+
+fav_num = 10
+message = f"my favorite number is { fav_num }"
+print(message)
+
+
